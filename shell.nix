5caf276e79d1a339b96e2d16441383fd664668c9
craigdallimore <decoy9697@gmail.com>
2022-08-29 20:45:55 +1200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,31 @@
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") { overlays = [ rust_overlay ]; };
+  rust_channel = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+in pkgs.mkShell {
+
+    name = "steering";
+
+    # Required for rust-analyzer?
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+    nativeBuildInputs = [
+      rust_channel # Full rust from overlay, includes cargo
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.wasm-pack          # A utility that builds rust-generated WebAssembly
+    ];
+
+    buildInputs = [
+      pkgs.rustfmt
+      pkgs.simple-http-server # For development
+    ];
+    shellHook = ''
+      alias gst='git status'
+      alias gd='git diff'
+      alias gc='git commit'
+      :q() {
+        exit
+      }
+    '';
+  }
