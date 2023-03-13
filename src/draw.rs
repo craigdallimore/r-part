4cbd34e61807ffdaf0fabb435b2572658e3dc208
craigdallimore <decoy9697@gmail.com>
2023-03-13 18:12:57 +1300

    Render with webgl

--- src/draw.rs
+++ src/draw.rs
@@ -1,40 +1,23 @@
-use crate::{State, particle::Particle};
-use web_sys::{WebGl2RenderingContext};
+//use crate::{State, particle::Particle};
+use crate::State;
+use web_sys::WebGl2RenderingContext;
 
 pub fn draw_scene(
   ctx: &web_sys::WebGl2RenderingContext,
   state: &State
 ) -> () {
 
-  /*
-  ctx.clear(
-    0.0,
-    0.0,
-    state.dimensions.0,
-    state.dimensions.1
+  ctx.viewport(
+    0,
+    0,
+    state.dimensions.0 as i32,
+    state.dimensions.1 as i32
   );
-  */
 
-  for p in state.emitter.particles.iter() {
-    draw_particle(ctx, &p);
-  }
+  ctx.clear_color(0.0, 0.0, 0.0, 0.0);
+  ctx.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
-}
-
-fn draw_particle(
-  ctx: &web_sys::WebGl2RenderingContext,
-  p: &Particle
-) -> () {
-
-  /*
-  ctx.save();
-  ctx.set_stroke_style(&"hsla(10, 10, 10, 1)".into());
-  ctx.begin_path();
-  ctx.move_to(p.last_position.0, p.last_position.1);
-  ctx.line_to(p.position.0, p.position.1);
-  ctx.stroke();
-  ctx.close_path();
-  ctx.restore();
-  */
+  ctx.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, 3)
 
 }
+
